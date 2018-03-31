3bce14b1ab445e8c7b2ee2dd071b7aaf6d1f6f20
klambycom <christian@klamby.com>
2018-04-01 01:23:06 +0200

    Refactor Range to function

--- apps/web/assets/js/Range.js
+++ apps/web/assets/js/Range.js
@@ -1,41 +1,22 @@
-import React, {Component} from "react";
+import React from "react";
+import Label from "./Label";
 
-class Range extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: props.max};
-  }
-
-  handleChange(e) {
-    this.setState({value: +e.target.value});
-    this.props.onChange(+e.target.value);
-  }
-
-  render() {
-    let valueTxt = this.state.value;
-    if (this.props.formatter) {
-      valueTxt = this.props.formatter(this.state.value);
-    }
-
-    return (
-      <div className="Range">
-        <div className="label">
-          <span>{this.props.label}</span>
-          <span className="value">{valueTxt}</span>
-        </div>
-        <div>
-          <input
-            type="range"
-            min={this.props.min}
-            max={this.props.max}
-            step={this.props.step}
-            value={this.state.value}
-            onChange={(e) => this.handleChange(e)}
-          />
-        </div>
+const Range = (props) => {
+  return (
+    <div className="Range">
+      <Label label={props.label} value={props.value} formatter={props.formatter} />
+      <div>
+        <input
+          type="range"
+          min={props.min}
+          max={props.max}
+          step={props.step}
+          value={props.value}
+          onChange={(e) => props.onChange(+e.target.value)}
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Range;
