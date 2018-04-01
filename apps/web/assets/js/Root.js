1979e7c2d88fc91c676584ea5c781d8218e46237
klambycom <christian@klamby.com>
2018-04-02 00:16:57 +0200

    Use Google Maps API key

--- apps/web/assets/js/Root.js
+++ apps/web/assets/js/Root.js
@@ -41,7 +41,7 @@ class Root extends Component {
         </div>
 
         <Map
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDtTZTBMSoEnrum5YU9JszzbX-y6MemEWs"
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `100vh` }} />}
           mapElement={<div style={{ height: `100%` }} />}
