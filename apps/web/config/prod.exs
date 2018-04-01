d97818831f86f249640bfc39d920e1f0488d96cb
klambycom <christian@klamby.com>
2018-04-01 22:45:13 +0200

    Setup distilery

--- apps/web/config/prod.exs
+++ apps/web/config/prod.exs
@@ -15,8 +15,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :web, Web.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "lghkarta.se", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: "."
 
 # ## SSL Support
 #
