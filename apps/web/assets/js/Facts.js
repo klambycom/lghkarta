d498aec33667939142ef9bc2496b0b684274f1df
klambycom <christian@klamby.com>
2018-04-06 23:36:54 +0200

    Fix facts

--- apps/web/assets/js/Facts.js
+++ apps/web/assets/js/Facts.js
@@ -6,20 +6,11 @@ const Facts = (props) => {
     <ul className="Facts">
       <li>Hyra {formatter.rent(props.rent)}</li>
       <li>{props.rooms} rum</li>
-      <li>{props.area}m2</li>
+      <li>{props.area} m²</li>
       <li>{props.landlord}</li>
       <li>{props.urban_area}</li>
     </ul>
   );
 };
-
-//{
-//  "rooms": "3",
-//  "area": "61",
-//  "landlord": "Lifra",
-//  "urban_area": "Norra Sofielund",
-//  "available_date": "2018-05-01 00:00:00+02:00 +02 Etc/GMT-2",
-//  "apply_before": "2018-03-27 23:59:00+02:00 +02 Etc/GMT-2"
-//}
 
 export default Facts;
