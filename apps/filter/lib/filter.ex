b6f81de848af25287de4d7a6aeab1b6705b0b700
klambycom <christian@klamby.com>
2018-05-27 22:05:16 +0200

    Filter apartments based on rent, nr of rooms and apartment type

--- apps/filter/lib/filter.ex
+++ apps/filter/lib/filter.ex
@@ -2,4 +2,43 @@ defmodule Filter do
   @moduledoc """
   Documentation for Filter.
   """
+  alias Apartment.Facts
+
+  @doc """
+  Filter apartments.
+
+  * `max_rent`, find all apartments cheaper than the max rent.
+  * `rooms`, find all apartments with the correct number of rooms. Can also find all apartments with more than, or equal to, n rooms
+  * `type`, find all apartments of selected types.
+
+  ## Example
+
+      iex> all_apartments()
+      iex> |> Filter.from_map(%{"max_rent" => 15000, "rooms" => {[2, 4], 8}, "type" => [:apartment]})
+      [
+        %Apartment{
+          facts: %Apartment.Facts{area: 86, rent: 9149, rooms: 4},
+          type: :apartment,
+          url: "https://www.boplatssyd.se/lagenhet/403854"
+        },
+        %Apartment{
+          facts: %Apartment.Facts{area: 62, rent: 7951, rooms: 2},
+          type: :apartment,
+          url: "https://www.boplatssyd.se/lagenhet/404414"
+        }
+      ]
+  """
+  def from_map(apartments, map), do: Enum.reduce(map, apartments, &filter/2)
+
+  defp filter({"max_rent", max_rent}, apartments),
+    do: apartments
+        |> Enum.filter(fn(%Apartment{facts: %Facts{rent: rent}}) -> rent <= max_rent end)
+
+  defp filter({"rooms", {values, max}}, apartments),
+    do: apartments
+        |> Enum.filter(fn(%Apartment{facts: %Facts{rooms: rooms}}) -> rooms in values || (max in values and rooms > max) end)
+
+  defp filter({"type", types}, apartments),
+    do: apartments
+        |> Enum.filter(fn(%Apartment{type: type}) -> type in types end)
 end
--- apps/filter/test/filter_test.exs
+++ apps/filter/test/filter_test.exs
@@ -1,4 +1,88 @@
 defmodule FilterTest do
   use ExUnit.Case
   doctest Filter
+
+  test "find all apartments cheaper than the max rent" do
+    assert Filter.from_map(all_apartments(), %{"max_rent" => 9500}) == [
+      %Apartment{
+        facts: %Apartment.Facts{area: 86, rent: 9149, rooms: 4},
+        type: :apartment,
+        url: "https://www.boplatssyd.se/lagenhet/403854"
+      },
+      %Apartment{
+        facts: %Apartment.Facts{area: 62, rent: 7951, rooms: 2},
+        type: :apartment,
+        url: "https://www.boplatssyd.se/lagenhet/404414"
+      }
+    ]
+  end
+
+  test "find all apartments with the correct number of rooms" do
+    assert Filter.from_map(all_apartments(), %{"rooms" => {[2, 3], 8}}) == [
+      %Apartment{
+        facts: %Apartment.Facts{area: 62, rent: 7951, rooms: 2},
+        type: :apartment,
+        url: "https://www.boplatssyd.se/lagenhet/404414"
+      },
+      %Apartment{
+        facts: %Apartment.Facts{area: 80, rent: 9778, rooms: 3},
+        type: :other,
+        url: "https://www.boplatssyd.se/lagenhet/404441"
+      }
+    ]
+  end
+
+  test "find all apartments with more than, or equal to, n rooms" do
+    assert Filter.from_map(all_apartments(), %{"rooms" => {[7, 8], 8}}) == [
+      %Apartment{
+        facts: %Apartment.Facts{area: 534, rent: 50049, rooms: 10},
+        type: :apartment,
+        url: "https://www.boplatssyd.se/lagenhet/404069"
+      }
+    ]
+  end
+
+  test "find all apartments of selected types" do
+    assert Filter.from_map(all_apartments(), %{"type" => [:senior, :other]}) == [
+      %Apartment{
+        facts: %Apartment.Facts{area: 104, rent: 11563, rooms: 4},
+        type: :senior,
+        url: "https://www.boplatssyd.se/lagenhet/404432"
+      },
+      %Apartment{
+        facts: %Apartment.Facts{area: 80, rent: 9778, rooms: 3},
+        type: :other,
+        url: "https://www.boplatssyd.se/lagenhet/404441"
+      }
+    ]
+  end
+
+  def all_apartments(),
+    do: [
+      %Apartment{
+        facts: %Apartment.Facts{area: 86, rent: 9149, rooms: 4},
+        type: :apartment,
+        url: "https://www.boplatssyd.se/lagenhet/403854"
+      },
+      %Apartment{
+        facts: %Apartment.Facts{area: 104, rent: 11563, rooms: 4},
+        type: :senior,
+        url: "https://www.boplatssyd.se/lagenhet/404432"
+      },
+      %Apartment{
+        facts: %Apartment.Facts{area: 62, rent: 7951, rooms: 2},
+        type: :apartment,
+        url: "https://www.boplatssyd.se/lagenhet/404414"
+      },
+      %Apartment{
+        facts: %Apartment.Facts{area: 80, rent: 9778, rooms: 3},
+        type: :other,
+        url: "https://www.boplatssyd.se/lagenhet/404441"
+      },
+      %Apartment{
+        facts: %Apartment.Facts{area: 534, rent: 50049, rooms: 10},
+        type: :apartment,
+        url: "https://www.boplatssyd.se/lagenhet/404069"
+      }
+    ]
 end
